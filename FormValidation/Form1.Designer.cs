60dc6d221ac3b4889e60d0c92a855ef85269e700
Alex <llecopower@gmail.com>
2022-10-06 08:30:06 -0500

    Add project files.

---
+++ FormValidation/Form1.Designer.cs
@@ -0,0 +1,96 @@
+﻿namespace FormValidation
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.labelID = new System.Windows.Forms.Label();
+            this.textBoxNumber = new System.Windows.Forms.TextBox();
+            this.listBoxNumber = new System.Windows.Forms.ListBox();
+            this.buttonAdd = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // labelID
+            // 
+            this.labelID.AutoSize = true;
+            this.labelID.Location = new System.Drawing.Point(82, 46);
+            this.labelID.Name = "labelID";
+            this.labelID.Size = new System.Drawing.Size(84, 16);
+            this.labelID.TabIndex = 0;
+            this.labelID.Text = "Type your ID";
+            // 
+            // textBoxNumber
+            // 
+            this.textBoxNumber.Location = new System.Drawing.Point(85, 80);
+            this.textBoxNumber.Name = "textBoxNumber";
+            this.textBoxNumber.Size = new System.Drawing.Size(174, 22);
+            this.textBoxNumber.TabIndex = 1;
+            // 
+            // listBoxNumber
+            // 
+            this.listBoxNumber.FormattingEnabled = true;
+            this.listBoxNumber.ItemHeight = 16;
+            this.listBoxNumber.Location = new System.Drawing.Point(85, 135);
+            this.listBoxNumber.Name = "listBoxNumber";
+            this.listBoxNumber.Size = new System.Drawing.Size(290, 132);
+            this.listBoxNumber.TabIndex = 2;
+            // 
+            // buttonAdd
+            // 
+            this.buttonAdd.Location = new System.Drawing.Point(275, 51);
+            this.buttonAdd.Name = "buttonAdd";
+            this.buttonAdd.Size = new System.Drawing.Size(100, 51);
+            this.buttonAdd.TabIndex = 3;
+            this.buttonAdd.Text = "&Add";
+            this.buttonAdd.UseVisualStyleBackColor = true;
+            this.buttonAdd.Click += new System.EventHandler(this.buttonAdd_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(457, 335);
+            this.Controls.Add(this.buttonAdd);
+            this.Controls.Add(this.listBoxNumber);
+            this.Controls.Add(this.textBoxNumber);
+            this.Controls.Add(this.labelID);
+            this.Name = "Form1";
+            this.Text = "ID Identification";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelID;
+        private System.Windows.Forms.TextBox textBoxNumber;
+        private System.Windows.Forms.ListBox listBoxNumber;
+        private System.Windows.Forms.Button buttonAdd;
+    }
+}
+
---
+++ FormValidation/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FormValidation
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonAdd_Click(object sender, EventArgs e)
+        {
+            bool valid = false;
+            int number = 0;
+            //at if the first statement it is always the TRUE conditional
+
+            if (!int.TryParse(textBoxNumber.Text,out number))
+            {
+                MessageBox.Show("The value must be numeric", "Validation");
+                valid = false;
+                textBoxNumber.Clear();
+                textBoxNumber.Focus();
+                return;
+            }
+
+            for (int i = 0; i <= listBoxNumber.Items.Count-1 ; i++)
+            {
+                //homework, tell WHY I put count-1 
+                // and TRY your BEST LOVE to not allow duplicate values
+                //at listbox
+            }
+            
+        }
+    }
+}
